ea03c33b39cffcc773d2d8e4abebbb642d536a4e
WA <sw955104@gmail.com>
2022-03-28 13:15:10 +0800

    測試 git push

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
+    <!-- <h1>{{ msg }}</h1> -->
+    <img :src="bird" v-if="isBird" alt="...">
+    <ul v-for="item in products" v-else :key="item.id">
+      <li>{{item.id}}</li>
+    </ul>
+    <!-- <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -27,15 +31,38 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
+const products = [
+  {
+    id: '123',
+    name: 'aassa'
+  },
+  {
+    id: '456',
+    name: 'aass456a'
+  }
+]
 export default {
+  data () {
+    return {
+      products: [],
+      bird: 'https://media.discordapp.net/attachments/825605859555016706/918477721372352522/image0.gif',
+      isBird: true
+    }
+  },
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted () {
+    setTimeout(() => {
+      this.products = products
+      this.isBird = false
+    }, 3000)
   }
 }
 </script>
